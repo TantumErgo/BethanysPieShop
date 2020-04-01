739699d536f61d9aca03fc7eec30afa53a3e9302
TantumErgo <9907196+TantumErgo@users.noreply.github.com>
2020-04-01 19:33:14 -0400

    Add project files.

---
+++ BethanysPieShop/Migrations/20200316222923_DescriptionEdits.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class DescriptionEdits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 5,
+                column: "ShortDescription",
+                value: "Merry Christmas with this pie!");
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 8,
+                column: "ShortDescription",
+                value: "Our fall favorite");
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 9,
+                column: "ShortDescription",
+                value: "Get the rhubarb goodness!");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 5,
+                column: "ShortDescription",
+                value: "Happy holidays with this pie!");
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 8,
+                column: "ShortDescription",
+                value: "Our Halloween favorite");
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 9,
+                column: "ShortDescription",
+                value: "My God, so sweet!");
+        }
+    }
+}
---
+++ BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; }
+        public string Content { get; set; } //2nd Content (parameter below) is this property
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; //i.e. an anchor tag
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content); //1st Content is the text of the anchor tag
+        }
+    }
+}
